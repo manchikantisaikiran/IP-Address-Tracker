20fdf5e92dddf1c26148e5d048bc059c7583ac30
manchikantisaikiran <manchikantisaikiran777@gmail.com>
2023-11-18 08:21:45 +0530

    regex bug fix

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,4 @@
-const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
-const domainRegex = /^(https?:\/\/)?(?:[a-zA-Z0-9.-]+|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/;
+const ipRegex = /^(?:(?:[0-9]{1,3}\.){3}[0-9]{1,3})$|^(?:[0-9a-fA-F]{1,4}(?::[0-9a-fA-F]{1,4}){0,7})$/;
+const domainRegex = /^(https?:\/\/)?([a-zA-Z0-9-]+\.){1,}[a-zA-Z]{2,}$/;
 const geoIpBaseUrl = 'https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_rWCwhOz8XFt6RpC7s5XZe06EpgLh1&';
 const localKeyForHistory = 'history';
